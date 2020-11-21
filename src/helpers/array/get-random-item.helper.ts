e5b3ec30caa1b65268016ef27e08ff58df7dcc4a
what1s1ove <zubko.2007@gmail.com>
2020-11-21 10:52:50 +0200

    module1-task3: + get random item helper

---
+++ src/helpers/array/get-random-item.helper.ts
@@ -0,0 +1,14 @@
+import {getRandomNumber} from '~/helpers/number';
+
+const RANDOM_ITEM_START_IDX = 0;
+const RANDOM_ITEM_DECREMENT = 1;
+
+const getRandomItem = <T>(items: T[]) => {
+  const randomIdx = getRandomNumber(RANDOM_ITEM_START_IDX, items.length - RANDOM_ITEM_DECREMENT);
+
+  const randomItem = items[randomIdx];
+
+  return randomItem;
+};
+
+export {getRandomItem};
