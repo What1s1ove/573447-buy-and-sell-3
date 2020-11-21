32ea5c96e9dd38f9ef0bf22478c13355e92ef10f
what1s1ove <zubko.2007@gmail.com>
2020-11-21 11:17:40 +0200

    module1-task3: * service root file

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,7 +1,29 @@
+import {CliCommandName, CliExitCode} from '~/common/enums';
+import {CLI_USER_ARGV_IDX} from '~/common/constants';
 import {cli} from './cli';
 
-cli.version.run();
-cli.help.run();
-console.log(cli.generate.run());
+const userArguments = process.argv.slice(CLI_USER_ARGV_IDX);
+const [userCommand] = userArguments;
 
-console.log(`Hello, world!`);
+switch (userCommand) {
+  case CliCommandName.VERSION: {
+    cli.version.run();
+
+    break;
+  }
+  case CliCommandName.HELP: {
+    cli.help.run();
+
+    break;
+  }
+  case CliCommandName.GENERATE: {
+    cli.generate.run();
+
+    break;
+  }
+  default: {
+    cli.help.run();
+
+    process.exit(CliExitCode.SUCCESS);
+  }
+}
