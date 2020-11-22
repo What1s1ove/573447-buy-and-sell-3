f50ae3b88f6dd3eadf7cbded6e632f6a633505f9
what1s1ove <zubko.2007@gmail.com>
2020-11-22 15:19:54 +0200

    module2-task1: * change proxy files

--- src/service/service.ts
+++ src/service/service.ts
@@ -1,6 +1,6 @@
 import {CliCommandName} from '~/common/enums';
 import {USER_ARGV_IDX, COMMAND_ARGS_IDX} from '~/common/constants';
-import {Cli} from './cli';
+import {Cli} from './cli/cli';
 
 const userArguments = process.argv.slice(USER_ARGV_IDX);
 const [userCommand] = userArguments;
