9d0a6d6d90f3cf3c5df28fce3ff42d49e374ef9e
what1s1ove <zubko.2007@gmail.com>
2020-11-23 19:52:47 +0200

    module2-task2: + data paths to config

--- src/service/cli/generate/common.ts
+++ src/service/cli/generate/common.ts
@@ -1,12 +1,18 @@
+const DATA_PATH = `./data`;
+
 const MocksConfig = {
   DEFAULT_COUNT: 1,
   MAX_COUNT: 1000,
   FILE_NAME: `mocks.json`,
+  TITLE: {
+    FILE_PATH: `${DATA_PATH}/titles.txt`,
+  },
   PICTURE_NUMBER: {
     MIN: 1,
     MAX: 16,
   },
   DESCRIPTION: {
+    FILE_PATH: `${DATA_PATH}/sentences.txt`,
     MIN_COUNT: 1,
     MAX_COUNT: 5,
   },
@@ -15,6 +21,7 @@ const MocksConfig = {
     MAX: 10000,
   },
   CATEGORY: {
+    FILE_PATH: `${DATA_PATH}/categories.txt`,
     MIN_COUNT: 1,
   },
 };
