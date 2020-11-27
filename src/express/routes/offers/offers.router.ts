f09ae73281729afa2af30e7eaff2d0962363fb15
what1s1ove <zubko.2007@gmail.com>
2020-11-27 20:02:48 +0200

    module3-task2: + render templates for offers router

--- src/express/routes/offers/offers.router.ts
+++ src/express/routes/offers/offers.router.ts
@@ -1,11 +1,11 @@
-import {Router, Request, Response} from 'express';
+import {Router} from 'express';
 import {ApiOffersRoute} from '~/common/enums';
 
 const offersRouter = Router();
 
-offersRouter.get(ApiOffersRoute.OFFER, (req: Request, res: Response) => res.send(req.path));
-offersRouter.get(ApiOffersRoute.EDIT, (req: Request, res: Response) => res.send(req.path));
-offersRouter.get(ApiOffersRoute.ADD, (req: Request, res: Response) => res.send(req.path));
-offersRouter.get(ApiOffersRoute.CATEGORY, (req: Request, res: Response) => res.send(req.path));
+offersRouter.get(ApiOffersRoute.OFFER, (_, res) => res.render(`category`));
+offersRouter.get(ApiOffersRoute.EDIT, (_, res) => res.render(`offers/new-ticket`));
+offersRouter.get(ApiOffersRoute.ADD, (_, res) => res.render(`offers/ticket-edit`));
+offersRouter.get(ApiOffersRoute.CATEGORY, (_, res) => res.render(`offers/ticket`));
 
 export default offersRouter;
