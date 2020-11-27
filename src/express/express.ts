1b5a54b2ff7e4613a56c8e8108e5ff8f60698c76
what1s1ove <zubko.2007@gmail.com>
2020-11-27 20:10:43 +0200

    module3-task2: * route for errors

--- src/express/express.ts
+++ src/express/express.ts
@@ -1,6 +1,6 @@
 import path from 'path';
-import express from 'express';
-import {ApiRoute} from '~/common/enums';
+import express, {Request, Response, NextFunction} from 'express';
+import {ApiRoute, HttpCode} from '~/common/enums';
 import mainRouter from '~/express/routes/main/main.router';
 import myRouter from '~/express/routes/my/my.router';
 import offersRouter from '~/express/routes/offers/offers.router';
@@ -15,6 +15,12 @@ app.use(ApiRoute.MY, myRouter);
 app.use(ApiRoute.OFFERS, offersRouter);
 
 app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+
+app.use((_, res) => res.status(HttpCode.BAD_REQUEST).render(`errors/404`));
+app.use((_err: Error, _req: Request, res: Response, _next: NextFunction) =>
+  res.status(HttpCode.INTERNAL_SERVER_ERROR).render(`errors/500`)
+);
+
 app.set(`views`, path.resolve(__dirname, `templates`));
 app.set(`view engine`, `pug`);
 
