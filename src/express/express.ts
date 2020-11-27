1369184d0ed4c2c071ac5b3d9be7ba8994f9385f
what1s1ove <zubko.2007@gmail.com>
2020-11-27 19:51:07 +0200

    module3-task2: + setup public folder

--- src/express/express.ts
+++ src/express/express.ts
@@ -1,9 +1,11 @@
+import path from 'path';
 import express from 'express';
 import {ApiRoute} from '~/common/enums';
 import mainRouter from '~/express/routes/main/main.router';
 import myRouter from '~/express/routes/my/my.router';
 import offersRouter from '~/express/routes/offers/offers.router';
 
+const PUBLIC_DIR = `public`;
 const DEFAULT_PORT = 8080;
 
 const app = express();
@@ -11,5 +13,9 @@ const app = express();
 app.use(ApiRoute.MAIN, mainRouter);
 app.use(ApiRoute.MY, myRouter);
 app.use(ApiRoute.OFFERS, offersRouter);
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
 
 app.listen(DEFAULT_PORT);
