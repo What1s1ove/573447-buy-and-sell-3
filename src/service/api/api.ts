b78b7a26d75a73ee75313e31c43a256728bb1529
what1s1ove <zubko.2007@gmail.com>
2020-12-07 19:57:42 +0200

    module4-task1: + init api functionality

---
+++ src/service/api/api.ts
@@ -0,0 +1,17 @@
+import {Router} from 'express';
+import {initCategoryApi} from './category/category';
+import {Category} from '~/service/data';
+import {getMockedDate} from './helpers';
+
+const app = Router();
+
+(async () => {
+  const mockedData = await getMockedDate();
+
+  initCategoryApi(
+    app,
+    new Category({
+      offers: mockedData,
+    })
+  );
+})();
---
+++ src/service/api/helpers.ts
@@ -0,0 +1,25 @@
+import {readFile} from '~/helpers';
+import {MOCKS_FILE_PATH} from '~/common/constants';
+import {IOffer} from '~/common/interfaces';
+
+let data: IOffer[] | null = null;
+
+const getMockedDate = async () => {
+  try {
+    if (data === null) {
+      const fileContent = await readFile(MOCKS_FILE_PATH);
+
+      data = JSON.parse(fileContent) as IOffer[];
+
+      return data;
+    }
+  } catch (err) {
+    console.log(err);
+
+    return Promise.reject(err);
+  }
+
+  return data;
+};
+
+export {getMockedDate};
