cbbc0adf13da114469c383f75724adc99b5d3249
what1s1ove <zubko.2007@gmail.com>
2020-12-08 10:08:18 +0200

    module4-task1: + get new offer helper

--- src/service/data/offers/helpers.ts
+++ src/service/data/offers/helpers.ts
@@ -1,4 +1,7 @@
+import {getRandomId} from '~/helpers';
+import {OfferKey} from '~/common/enums';
 import {IOffer} from '~/common/interfaces';
+import {CreatedOffer} from '~/common/types';
 
 const getOfferById = (offers: IOffer[], id: string) => {
   const offerById = offers.find((offer) => offer.id === id);
@@ -6,4 +9,14 @@ const getOfferById = (offers: IOffer[], id: string) => {
   return offerById ?? null;
 };
 
-export {getOfferById};
+const getNewOffer = (offer: CreatedOffer) => {
+  const newOffer: IOffer = {
+    ...offer,
+    [OfferKey.ID]: getRandomId(),
+    [OfferKey.COMMENTS]: [],
+  };
+
+  return newOffer;
+};
+
+export {getOfferById, getNewOffer};
