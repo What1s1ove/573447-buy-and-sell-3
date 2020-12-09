37a73256a253f67b76eaf530de020dfb4d2e982c
what1s1ove <zubko.2007@gmail.com>
2020-12-09 19:49:00 +0200

    module4-task1: + get item by key helper

--- src/helpers/array/index.ts
+++ src/helpers/array/index.ts
@@ -2,3 +2,5 @@ export * from './get-shuffled-items.helper';
 export * from './get-random-items.helper';
 export * from './get-random-item.helper';
 export * from './get-unique-items.helper';
+export * from './get-item-by-key.helper';
+export * from './remove-item-by-key.helper';
