3fef186621903102276b5e7fcd099e2a0376cc92
what1s1ove <zubko.2007@gmail.com>
2020-12-10 07:02:16 +0200

    module4-task1: * check is valid by key helper types

--- src/helpers/validation/check-is-valid-keys.helper.ts
+++ src/helpers/validation/check-is-valid-keys.helper.ts
@@ -1,6 +1,6 @@
 const checkIsValidByKeys = <T>(
   entity: T,
-  keys: ReadonlyArray<keyof T>
+  keys: ReadonlyArray<keyof T> | Array<keyof T>
 ): boolean => {
   const entityKeys = Object.keys(entity) as Array<keyof T>;
   const isValid = entityKeys.every((key) => keys.includes(key));
