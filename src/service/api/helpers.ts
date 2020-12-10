09867f57c640fb0d97f23701c35a11791c0c8df9
what1s1ove <zubko.2007@gmail.com>
2020-12-10 12:00:00 +0200

    module4-task1: * mocked data functionality

--- src/service/api/helpers.ts
+++ src/service/api/helpers.ts
@@ -2,11 +2,11 @@ import {readFile} from '~/helpers';
 import {MOCKS_FILE_PATH} from '~/common/constants';
 import {IOffer} from '~/common/interfaces';
 
-let data: IOffer[] | null = null;
+let data: IOffer[] = [];
 
 const getMockedDate = async (): Promise<IOffer[]> => {
   try {
-    if (data === null) {
+    if (!data.length) {
       const fileContent = await readFile(MOCKS_FILE_PATH);
 
       data = JSON.parse(fileContent) as IOffer[];
