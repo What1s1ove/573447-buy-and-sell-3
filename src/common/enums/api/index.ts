0ff1bd346103429dc574f1f7cd1e6ebc844ab86b
what1s1ove <zubko.2007@gmail.com>
2020-12-15 20:25:35 +0200

    module4-task3: + http success code enum

--- src/common/enums/api/index.ts
+++ src/common/enums/api/index.ts
@@ -3,3 +3,5 @@ export * from './api-path.enum';
 export * from './category-api-path.enum';
 export * from './offers-api-path.enum';
 export * from './search-api-path.enum';
+export * from './http-method.enum';
+export * from './success-http-status-range.enum';
