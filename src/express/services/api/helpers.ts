886a6ff3115751c737baab3490a580c72c741f2d
what1s1ove <zubko.2007@gmail.com>
2020-12-16 09:18:30 +0200

    module4-task3: * check is ok helper

--- src/express/services/api/helpers.ts
+++ src/express/services/api/helpers.ts
@@ -1,7 +1,7 @@
 import { HttpCode, SuccessHTTPStatusRange } from '~/common/enums';
 
 const checkIsOkStatusCode = (statusCode: HttpCode): boolean => {
-  const isOk = statusCode < SuccessHTTPStatusRange.MIN && statusCode > SuccessHTTPStatusRange.MAX;
+  const isOk = statusCode >= SuccessHTTPStatusRange.MIN && statusCode <= SuccessHTTPStatusRange.MAX;
 
   return isOk;
 };
