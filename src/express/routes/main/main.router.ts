fd09ebd3901e7a2f30c10d0ea3d3fd9ea5293594
what1s1ove <zubko.2007@gmail.com>
2020-12-17 19:34:28 +0200

    module4-task3: + search route

--- src/express/routes/main/main.router.ts
+++ src/express/routes/main/main.router.ts
@@ -8,16 +8,32 @@ const initMainRouter = (app: Router, settings: SsrRouterSettings): void => {
 
   app.use(SsrPath.MAIN, mainRouter);
 
-  mainRouter.get(SsrMainPath.ROOT, async (_, res): Promise<void> => {
-    const offers = await api.getOffers();
+  mainRouter.get(
+    SsrMainPath.ROOT,
+    async (_, res): Promise<void> => {
+      const offers = await api.getOffers();
 
-    res.render(`main`, {
-      items: offers,
-    });
-  });
+      return res.render(`main`, {
+        items: offers,
+      });
+    },
+  );
+
   mainRouter.get(SsrMainPath.REGISTER, (_, res) => res.render(`sign-up`));
+
   mainRouter.get(SsrMainPath.LOGIN, (_, res) => res.render(`login`));
-  mainRouter.get(SsrMainPath.SEARCH, (_, res) => res.render(`search-result`));
+
+  mainRouter.get(
+    SsrMainPath.SEARCH,
+    async (req, res): Promise<void> => {
+      const { search } = req.query;
+      const resultItems = await api.search(search as string);
+
+      return res.render(`search-result`, {
+        resultItems,
+      });
+    },
+  );
 };
 
 export { initMainRouter };
