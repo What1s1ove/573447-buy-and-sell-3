03c7bfd9dd77ed29062ed0bca2632d4eb6ef6479
what1s1ove <zubko.2007@gmail.com>
2020-12-18 07:35:59 +0200

    module4-task3: * init offer router

--- src/express/routes/offers/offers.router.ts
+++ src/express/routes/offers/offers.router.ts
@@ -1,11 +1,38 @@
 import { Router } from 'express';
-import { SsrOffersPath } from '~/common/enums';
+import { SsrOffersPath, SsrPath } from '~/common/enums';
+import { SsrRouterSettings } from '~/express/common';
 
-const offersRouter = Router();
+const initOffersRouter = (app: Router, settings: SsrRouterSettings): void => {
+  const offersRouter = Router();
+  const { api } = settings;
 
-offersRouter.get(SsrOffersPath.$OFFER_ID, (_, res) => res.render(`category`));
-offersRouter.get(SsrOffersPath.EDIT_$OFFER_ID, (_, res) => res.render(`offers/new-ticket`));
-offersRouter.get(SsrOffersPath.ADD, (_, res) => res.render(`offers/ticket-edit`));
-offersRouter.get(SsrOffersPath.CATEGORY_$CATEGORY_ID, (_, res) => res.render(`offers/ticket`));
+  app.use(SsrPath.OFFERS, offersRouter);
 
-export default offersRouter;
+  offersRouter.get(SsrOffersPath.CATEGORY_$CATEGORY_ID, (_, res) => {
+    return res.render(`category`);
+  });
+
+  offersRouter.get(SsrOffersPath.ADD, (_, res) => {
+    return res.render(`offers/new-ticket`);
+  });
+
+  offersRouter.get(SsrOffersPath.EDIT_$OFFER_ID, async (req, res) => {
+    const { id } = req.params;
+
+    const [offer, categories] = await Promise.all([
+      api.getOffer(id),
+      api.getCategories(),
+    ]);
+
+    return res.render(`offers/ticket-edit`, {
+      offer,
+      categories,
+    });
+  });
+
+  offersRouter.get(SsrOffersPath.$OFFER_ID, (_, res) => {
+    return res.render(`offers/ticket`);
+  });
+};
+
+export { initOffersRouter };
