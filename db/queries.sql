26f378fcf84fd8daf171eb17b76d741690d9c0a0
what1s1ove <zubko.2007@gmail.com>
2020-12-29 09:03:38 +0200

    module5-task3: + categoris with one offer

--- db/queries.sql
+++ db/queries.sql
@@ -1,2 +1,15 @@
 /* All categories */
 SELECT * FROM categories;
+
+/* Categories with at least one offer  */
+SELECT
+  categories.name,
+  offers_categories.category_id
+FROM
+  offers_categories
+  INNER JOIN categories
+    ON offers_categories.category_id = categories.id
+GROUP BY
+  categories.name,
+  offers_categories.category_id
+HAVING count(offers_categories) > 1;
