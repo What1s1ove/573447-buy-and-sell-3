7eba70e9f2789054dad644a1b74a628218fe9678
what1s1ove <zubko.2007@gmail.com>
2020-12-29 21:04:19 +0200

    module5-task3: + categories with offer count sql

--- db/queries.sql
+++ db/queries.sql
@@ -13,3 +13,16 @@ GROUP BY
   categories.name,
   offers_categories.category_id
 HAVING count(offers_categories) > 1;
+
+/* Categories with offer count */
+SELECT
+  categories.name,
+  offers_categories.category_id,
+  count(offers_categories.offer_id) as "offers_count"
+FROM
+  offers_categories
+  INNER JOIN  categories
+    ON offers_categories.category_id = categories.id
+GROUP BY
+  categories.name,
+  offers_categories.category_id;
