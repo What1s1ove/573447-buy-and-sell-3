4d230f9567d28eb421807ca81a9ff5da848f4e7b
what1s1ove <zubko.2007@gmail.com>
2021-01-09 14:40:40 +0200

    module6-task2: * init api functionality

--- src/service/api/api.ts
+++ src/service/api/api.ts
@@ -6,39 +6,34 @@ import { DbModels } from '~/common/types';
 import { initCategoryApi } from './category/category';
 import { initOffersApi } from './offers/offers';
 import { initSearchApi } from './search/search';
-import { getMockedDate } from './helpers';
 
 const apiRouter = Router();
 
-(async (): Promise<void> => {
-  defineModels(sequelize);
+defineModels(sequelize);
 
-  const models = sequelize.models as DbModels;
+const models = sequelize.models as DbModels;
 
-  const mockedData = await getMockedDate();
+initCategoryApi(
+  apiRouter,
+  new Category({
+    categoryModel: models.Category,
+  }),
+);
 
-  initCategoryApi(
-    apiRouter,
-    new Category({
-      categoryModel: models.Category,
-    }),
-  );
+initOffersApi(apiRouter, {
+  offers: new Offers({
+    offerModel: models.Offer,
+  }),
+  comments: new Comments({
+    commentModel: models.Comment,
+  }),
+});
 
-  initOffersApi(apiRouter, {
-    offers: new Offers({
-      offers: mockedData,
-    }),
-    comments: new Comments({
-      commentModel: models.Comment,
-    }),
-  });
-
-  initSearchApi(
-    apiRouter,
-    new Search({
-      offerModel: models.Offer,
-    }),
-  );
-})();
+initSearchApi(
+  apiRouter,
+  new Search({
+    offerModel: models.Offer,
+  }),
+);
 
 export default apiRouter;
