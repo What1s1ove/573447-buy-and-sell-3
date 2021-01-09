a6ad74cfb4df56c26eecb104b7ea7bdf80b1f2c7
what1s1ove <zubko.2007@gmail.com>
2021-01-09 14:23:50 +0200

    module6-task2: * comment interface

--- src/common/enums/comment/comment-key.enum.ts
+++ src/common/enums/comment/comment-key.enum.ts
@@ -1,6 +1,8 @@
 enum CommentKey {
   ID = `id`,
   TEXT = `text`,
+  CREATED_AT = `createdAt`,
+  UPDATED_AT = `updatedAt`,
 }
 
 export { CommentKey };
--- src/common/interfaces/comment/comment.interface.ts
+++ src/common/interfaces/comment/comment.interface.ts
@@ -1,8 +1,10 @@
 import { CommentKey } from '~/common/enums';
 
 interface IComment {
-  [CommentKey.ID]: string;
+  [CommentKey.ID]: number;
   [CommentKey.TEXT]: string;
+  [CommentKey.CREATED_AT]: string;
+  [CommentKey.UPDATED_AT]: string;
 }
 
 export { IComment };
