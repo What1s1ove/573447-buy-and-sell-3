23ea6ac34bd85a08473717ea0b01fdfe97678f89
what1s1ove <zubko.2007@gmail.com>
2021-01-13 20:23:00 +0200

    module6-task3: + find page method

--- src/service/data/offers/offers.ts
+++ src/service/data/offers/offers.ts
@@ -1,6 +1,6 @@
 import { ModelAlias, OfferKey } from '~/common/enums';
 import { IOffer } from '~/common/interfaces';
-import { CreatedOffer, OfferModel } from '~/common/types';
+import { CreatedOffer, OfferModel, OffersWithCount, PaginationPayload } from '~/common/types';
 
 type Constructor = {
   offerModel: OfferModel;
@@ -23,6 +23,24 @@ class Offers {
     });
 
     return offers.map((item) => item.get());
+  }
+
+  async findPage({ limit, offset }: PaginationPayload): Promise<OffersWithCount> {
+    const { count, rows } = await this.#Offer.findAndCountAll({
+      limit,
+      offset,
+      include: [
+        ModelAlias.CATEGORIES,
+        ModelAlias.COMMENTS,
+        ModelAlias.OFFER_TYPES,
+      ],
+      distinct: true,
+    });
+
+    return {
+      count,
+      offers: rows.map((item) => item.get()),
+    };
   }
 
   public findOne(id: IOffer[OfferKey.ID]): Promise<IOffer | null> {
