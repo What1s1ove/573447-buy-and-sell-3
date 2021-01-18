96c0fd6a0114837ae3653275c757f06c8653d0b2
what1s1ove <zubko.2007@gmail.com>
2021-01-18 20:09:06 +0200

    module7-task1: + error response type

--- src/common/types/api/index.ts
+++ src/common/types/api/index.ts
@@ -3,3 +3,4 @@ import { Request, Response, NextFunction } from 'express';
 export { Request, Response, NextFunction };
 export * from './offers';
 export * from './comments';
+export * from './response';
---
+++ src/common/types/api/response/error-response.type.ts
@@ -0,0 +1,5 @@
+type ErrorResponse = {
+  messages: string[];
+};
+
+export { ErrorResponse };
