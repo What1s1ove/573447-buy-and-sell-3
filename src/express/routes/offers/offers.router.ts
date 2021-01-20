4262033248da67cdbce73cc39efc4b263f75dfe4
what1s1ove <zubko.2007@gmail.com>
2021-01-20 09:25:40 +0200

    module7-task1: + async handler for offers router

--- src/express/routes/offers/offers.router.ts
+++ src/express/routes/offers/offers.router.ts
@@ -4,7 +4,12 @@ import { Router } from 'express';
 import multer from 'multer';
 import { SsrOffersPath, SsrPath } from '~/common/enums';
 import { SsrRouterSettings } from '~/express/common';
-import { getFileExtension, getHttpErrors, getRandomId } from '~/helpers';
+import {
+  getFileExtension,
+  getHttpErrors,
+  getRandomId,
+  asyncHandler,
+} from '~/helpers';
 import { getOfferData } from './helpers';
 
 const UPLOAD_DIR = `../../upload/img/`;
@@ -33,19 +38,22 @@ const initOffersRouter = (app: Router, settings: SsrRouterSettings): void => {
     return res.render(`category`);
   });
 
-  offersRouter.get(SsrOffersPath.ADD, async (_, res) => {
-    const categories = await api.getCategories();
+  offersRouter.get(
+    SsrOffersPath.ADD,
+    asyncHandler(async (_, res) => {
+      const categories = await api.getCategories();
 
-    return res.render(`offers/ticket-edit`, {
-      offer: {},
-      categories,
-    });
-  });
+      return res.render(`offers/ticket-edit`, {
+        offer: {},
+        categories,
+      });
+    }),
+  );
 
   offersRouter.post(
     SsrOffersPath.ADD,
     upload.single(`avatar`),
-    async (req, res) => {
+    asyncHandler(async (req, res) => {
       const { body, file } = req;
       const offerData = getOfferData(body, file?.filename);
 
@@ -62,26 +70,29 @@ const initOffersRouter = (app: Router, settings: SsrRouterSettings): void => {
           errorMessages: getHttpErrors(err),
         });
       }
-    },
+    }),
   );
 
-  offersRouter.get(SsrOffersPath.EDIT_$OFFER_ID, async (req, res) => {
-    const { id } = req.params;
-    const [offer, categories] = await Promise.all([
-      api.getOffer(Number(id)),
-      api.getCategories(),
-    ]);
+  offersRouter.get(
+    SsrOffersPath.EDIT_$OFFER_ID,
+    asyncHandler(async (req, res) => {
+      const { id } = req.params;
+      const [offer, categories] = await Promise.all([
+        api.getOffer(Number(id)),
+        api.getCategories(),
+      ]);
 
-    return res.render(`offers/ticket-edit`, {
-      offer,
-      categories,
-    });
-  });
+      return res.render(`offers/ticket-edit`, {
+        offer,
+        categories,
+      });
+    }),
+  );
 
   offersRouter.post(
     SsrOffersPath.EDIT_$OFFER_ID,
     upload.single(`avatar`),
-    async (req, res) => {
+    asyncHandler(async (req, res) => {
       const { body, file, params } = req;
       const offerData = getOfferData(body, file?.filename);
 
@@ -98,37 +109,43 @@ const initOffersRouter = (app: Router, settings: SsrRouterSettings): void => {
           errorMessages: getHttpErrors(err),
         });
       }
-    },
+    }),
   );
 
-  offersRouter.get(SsrOffersPath.$OFFER_ID, async (req, res) => {
-    const { id } = req.params;
-    const offer = await api.getOffer(Number(id));
-
-    return res.render(`offers/ticket`, {
-      item: offer,
-    });
-  });
-
-  offersRouter.post(SsrOffersPath.$OFFER_ID_COMMENT, async (req, res) => {
-    const { body, params } = req;
-    const { id } = params;
-
-    try {
-      await api.createComment(Number(id), {
-        text: body.comment,
-      });
-
-      return res.redirect(`${SsrPath.OFFERS}/${id}`);
-    } catch (err: unknown) {
+  offersRouter.get(
+    SsrOffersPath.$OFFER_ID,
+    asyncHandler(async (req, res) => {
+      const { id } = req.params;
       const offer = await api.getOffer(Number(id));
 
       return res.render(`offers/ticket`, {
         item: offer,
-        errorMessages: getHttpErrors(err),
       });
-    }
-  });
+    }),
+  );
+
+  offersRouter.post(
+    SsrOffersPath.$OFFER_ID_COMMENT,
+    asyncHandler(async (req, res) => {
+      const { body, params } = req;
+      const { id } = params;
+
+      try {
+        await api.createComment(Number(id), {
+          text: body.comment,
+        });
+
+        return res.redirect(`${SsrPath.OFFERS}/${id}`);
+      } catch (err: unknown) {
+        const offer = await api.getOffer(Number(id));
+
+        return res.render(`offers/ticket`, {
+          item: offer,
+          errorMessages: getHttpErrors(err),
+        });
+      }
+    }),
+  );
 };
 
 export { initOffersRouter };
