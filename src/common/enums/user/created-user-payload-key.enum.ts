87d67ac10e721f4b2703faf9c1f3d1120f0de92e
what1s1ove <zubko.2007@gmail.com>
2021-01-23 15:54:21 +0200

    module7-task2: + created user payload type

---
+++ src/common/enums/user/created-user-payload-key.enum.ts
@@ -0,0 +1,9 @@
+enum CreatedUserPayloadKey {
+  EMAIL = `email`,
+  PASSWORD = `password`,
+  REPEATED_PASSWORD = `repeatedPassword`,
+  FIRST_NAME = `firstName`,
+  LAST_NAME = `lastName`,
+}
+
+export { CreatedUserPayloadKey };
---
+++ src/common/types/user/created-user-payload.type.ts
@@ -0,0 +1,11 @@
+import { CreatedUserPayloadKey } from '~/common/enums';
+
+type CreatedUserPayload = {
+  [CreatedUserPayloadKey.EMAIL]: string;
+  [CreatedUserPayloadKey.PASSWORD]: string;
+  [CreatedUserPayloadKey.REPEATED_PASSWORD]: string;
+  [CreatedUserPayloadKey.FIRST_NAME]: string;
+  [CreatedUserPayloadKey.LAST_NAME]: string;
+};
+
+export { CreatedUserPayload };
