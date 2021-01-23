49d046bb0c4daa34b8df69914c97db20d0d9f2b9
what1s1ove <zubko.2007@gmail.com>
2021-01-23 14:47:07 +0200

    module7-task2: - validation helpers

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -2,7 +2,6 @@ export * from './number';
 export * from './string';
 export * from './array';
 export * from './fs';
-export * from './validation';
 export * from './log';
 export * from './file';
 export * from './mocks';
