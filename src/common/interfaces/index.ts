2155edda4c606685fe2743ee01a7867c81437015
what1s1ove <zubko.2007@gmail.com>
2021-01-23 14:55:42 +0200

    module7-task2: * user interface

--- src/common/interfaces/index.ts
+++ src/common/interfaces/index.ts
@@ -1,3 +1,4 @@
 export * from './offer';
 export * from './comment';
 export * from './category';
+export * from './user';
---
+++ src/common/interfaces/user/user.interface.ts
@@ -0,0 +1,13 @@
+import { UserKey } from '~/common/enums';
+
+interface IUser {
+  [UserKey.ID]: number;
+  [UserKey.EMAIL]: string;
+  [UserKey.PASSWORD]: string;
+  [UserKey.FIRST_NAME]: string;
+  [UserKey.LAST_NAME]: string;
+  [UserKey.CREATED_AT]: string;
+  [UserKey.UPDATED_AT]: string;
+}
+
+export { IUser };
