5c28a558ef756938fe209fdd13a5d6b429d2d448
what1s1ove <zubko.2007@gmail.com>
2021-01-24 14:55:18 +0200

    module7-task3: + user login payload key

--- src/common/enums/user/index.ts
+++ src/common/enums/user/index.ts
@@ -1,3 +1,4 @@
 export * from './user-dto-key.enum';
 export * from './user-key.enum';
 export * from './created-user-payload-key.enum';
+export * from './user-login-payload-key.enum';
---
+++ src/common/enums/user/user-login-payload-key.enum.ts
@@ -0,0 +1,6 @@
+enum UserLoginPayloadKey {
+  EMAIL = `email`,
+  PASSWORD = `password`,
+}
+
+export { UserLoginPayloadKey };
